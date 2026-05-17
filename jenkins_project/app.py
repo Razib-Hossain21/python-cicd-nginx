5e97afe7df19b4f7bc3f0dfce20da9025731d036
Razib-Hussain21 <razib.cse1617@gmail.com>
2026-05-17 11:41:16 +0600

    updates dockerfiel

--- jenkins_project/app.py
+++ jenkins_project/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Python App Running Through NGINX + Docker + Jenkins CI/CD"
+    return "Hello Mr. Razib, you have successfully deployed. Congratulations..! Python App Running Through NGINX + Docker + Jenkins CI/CD"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
